f3d1b7366604953deb22723f46500d62266b2d08
par0v0zik <par0v0zik831990@yandex.ru>
2014-07-06 14:28:07 +0300

    Custom timezone v.1

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,18 +1,19 @@
 class ApiController < ApplicationController
   def time
 
-  	# Set +0300 time zone
-  	Time.zone = "Moscow"
-  	# Get standard datetime from service, using HTTParty
-  	response = HTTParty.get('http://time.jsontest.com/').as_json
-  	# Parse response, using mask
-  	standard_time = Time.strptime("#{response["date"]} #{response["time"]} UTC", "%m-%d-%Y %H:%M:%S %P %Z")
-  	# Convert stadart datetime to current timezone
-  	moscow_time = standard_time.in_time_zone
-  	# Format datetime to required
-  	formatted_time = moscow_time.strftime("%Y-%m-%d %H:%M:%S")
+	  	# Set given timezone or Moscow as default
+	  	Time.zone = params[:timezone] || "Moscow"
+	  	# Get standard datetime from service, using HTTParty
+	  	response = HTTParty.get('http://time.jsontest.com/').as_json
+	  	# Parse response, using mask
+	  	standard_time = Time.strptime("#{response["date"]} #{response["time"]} UTC", "%m-%d-%Y %H:%M:%S %P %Z")
+	  	# Convert stadart datetime to current timezone
+	  	moscow_time = standard_time.in_time_zone
+	  	# Format datetime to required
+	  	formatted_time = moscow_time.strftime("%Y-%m-%d %H:%M:%S")
+	  	render json: {:datetime => formatted_time}.to_json
 
-  	render json: {:datetime => formatted_time}.to_json
-	
+	rescue ArgumentError
+		render json: {:error => "Invalid timezone declaration!"}.to_json
   end
 end
