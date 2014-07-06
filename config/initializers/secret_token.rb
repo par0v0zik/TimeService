d5ee21f36c841175c41b220f4511473ba4a47634
par0v0zik <par0v0zik831990@yandex.ru>
2014-07-06 12:36:27 +0300

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TimeService::Application.config.secret_key_base = '62a6388c52e77a2aef9ef9cfa6476f74077ac563badd1d2d4618f999ce00ca9089a4667d11ce859540c1c4774941e388799a7e5c086662806eb2c158c1c61e1f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TimeService::Application.config.session_store :cookie_store, key: '_TimeService_session'
